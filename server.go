ebfac73f52b76d849c0757427c6400f8d72077ff
Liande Guo <104030726+liandeguo@users.noreply.github.com>
2024-07-19 13:50:25 +0200

    added more things

--- server.go
+++ server.go
@@ -3,16 +3,36 @@ package main
 import (
 	"net/http"
 
-	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/hello", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello World")
-	})
 	e.GET("/user", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Skibidi World")
+		id := c.QueryParam("id")
+
+		return c.String(http.StatusOK, id)
+	})
+	e.GET("/user/heatmap", func(c echo.Context) error {
+		// This should give the days when the user has played like the github contribution graph
+		id := c.QueryParam("id")
+
+		response := "Hello" + id
+		return c.String(http.StatusOK, response)
+	})
+	e.GET("/user/trend/trophies", func(c echo.Context) error {
+		// This should give the trophies from the user since the first sign up on the site. To make a graph of the trophies
+		// id := c.QueryParam("id")
+
+		return c.String(http.StatusOK, "Hello World")
 	})
 	e.Logger.Fatal(e.Start(":8123"))
 }
+
+// func database() {
+// 	db, err := sql.Open("sqlite3", "./test.db")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer db.Close()
+// }
